4f2e5eb35134fe8d4a8da96b25e5ec2959f31eec
MGCA2021 <78860973+MGCA2021@users.noreply.github.com>
2021-03-16 22:05:10 -0300

    Update ArrayIndexListTest.java

--- ac2/src/tadListaArranjo/ArrayIndexListTest.java
+++ ac2/src/tadListaArranjo/ArrayIndexListTest.java
@@ -19,34 +19,40 @@ class ArrayIndexListTest {
 		A.add(0, 7);
 		assertEquals("(7)", A.toString());
 		
-		A.add(0, 4);
-		assertEquals("(4, 7)", A.toString());
+		A.add(0, 11);
+		assertEquals("(11, 7)", A.toString());
 		
-		assertEquals(7, A.get(1));
+		assertEquals(11, A.get(0));
 		
-		A.add(2, 2);
-		assertEquals("(4, 7, 2)", A.toString());
+		A.add(1, 8);
+		assertEquals("(11, 8, 7)", A.toString());
 		
-		assertThrows(IndexOutOfBoundsException.class, () -> { A.get(3);});
-		assertEquals(7, A.remove(1));
-		assertEquals("(4, 2)", A.toString());
+		A.add(2, 15);
+		assertEquals("(11, 8, 15, 7)", A.toString());
 		
-		A.add(1, 5);
-		assertEquals("(4, 5, 2)", A.toString());
+//----------------------------------------------------------------------------------		
+		assertThrows(IndexOutOfBoundsException.class, () -> { A.get(4);});
+//----------------------------------------------------------------------------------	
+		
+		assertEquals(8, A.remove(1));
+		assertEquals("(11, 15, 7)", A.toString());
+//----------------------------------------------------------------------------------	
+		A.add(1, 13);
+		assertEquals("(11, 13, 15, 7)", A.toString());
 		
 		A.add(1, 3);
-		assertEquals("(4, 3, 5, 2)", A.toString());
+		assertEquals("(11, 3, 13, 15, 7)", A.toString());
 		
 		A.add(4, 9);
-		assertEquals("(4, 3, 5, 2, 9)", A.toString());
+		assertEquals("(11, 3, 13, 15, 9, 7)", A.toString());
 		
-		assertEquals(5, A.get(2));
-		assertEquals("(4, 3, 5, 2, 9)", A.toString());
+		assertEquals(15, A.get(3));
+		assertEquals("(11, 3, 13, 15, 9, 7)", A.toString());
 		
-		assertEquals(2, A.set(3, 8));
-		assertEquals("(4, 3, 5, 8, 9)", A.toString());
+		assertEquals(15, A.set(3, 9));
+		assertEquals("(11, 3, 13, 9, 9, 7)", A.toString());
 		
 		}
-
+	
 	}
 
